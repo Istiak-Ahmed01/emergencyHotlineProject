5b3097a482fca1c85ce809a5063df61134f28573
Istiak-Ahmed01 <istiakefty1@gmail.com>
2025-09-11 23:42:17 +0600

    added call history dinamically

--- script.js
+++ script.js
@@ -12,18 +12,34 @@ document.querySelectorAll(".cardBtn").forEach((btn) => {
         const card = btn.closest(".card")
         const serviceName = card.querySelector(".serviceName").innerText
         const serviceNumber = card.querySelector(".serviceNumber").innerText
-        alert(`📞 Calling ${serviceName} ${serviceNumber}...`)
 
         // coins
-    const coins = document.getElementById("coins")
-    const currCoins = parseInt(coins.innerHTML)
-    const totalCoins = parseInt(currCoins - 20)
-    if(totalCoins >= 0){
-    coins.innerHTML = totalCoins
+        const coins = document.getElementById("coins")
+        const currCoins = parseInt(coins.innerHTML)
+        const totalCoins = parseInt(currCoins - 20)
+        if (totalCoins >= 0) {
+            alert(`📞 Calling ${serviceName} ${serviceNumber}...`)
 
-    }else{
-        alert(`❌আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে 20 কয়েন লাগবে।`)
-    }
+            coins.innerHTML = totalCoins
+
+            //call history functionality
+            const callHistory = document.getElementById("carHistory")
+            const newHistory = document.createElement("div")
+
+            newHistory.innerHTML = `<div class="flex items-center justify-between bg-[#fafafa] h-20 p-4 mt-4">
+
+         <div  class="p-4">
+                        <h3 class="text-lg font-semibold">${serviceName}</h3>
+                        <p class="hind text-lg font-normal text-[#5C5C5C]">${serviceNumber}</p>
+                    </div>
+                    <p class="hind text-lg font-normal ">11:36:58 AM</p>
+                    </div>`
+            callHistory.appendChild(newHistory)
+
+        } else {
+            alert(`❌আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে 20 কয়েন লাগবে।`)
+        }
+
 
     })
 
