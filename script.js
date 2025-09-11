ebc6953258dc491c77ae433efb73d110f825e2d5
Istiak-Ahmed01 <istiakefty1@gmail.com>
2025-09-11 23:20:17 +0600

    add coin functioality

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ document.querySelectorAll(".cardBtn").forEach((btn) => {
         const serviceNumber = card.querySelector(".serviceNumber").innerText
         alert(`📞 Calling ${serviceName} ${serviceNumber}...`)
 
+        // coins
+    const coins = document.getElementById("coins")
+    const currCoins = parseInt(coins.innerHTML)
+    const totalCoins = parseInt(currCoins - 20)
+    if(totalCoins >= 0){
+    coins.innerHTML = totalCoins
+
+    }else{
+        alert(`❌আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে 20 কয়েন লাগবে।`)
+    }
+
     })
 
 })
