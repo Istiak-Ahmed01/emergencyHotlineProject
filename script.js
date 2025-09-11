30ac6598f3e5468104b40d217507d60c53c09cba
Istiak-Ahmed01 <istiakefty1@gmail.com>
2025-09-11 19:52:39 +0600

    added calling alert

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const addFavNumber = () => {
-const favNumber = document.getElementById("favNumber")
-const currFavNumber = parseInt(favNumber.innerHTML)
-const totalFavNumber = currFavNumber + 1
-favNumber.innerHTML = totalFavNumber
+    const favNumber = document.getElementById("favNumber")
+    const currFavNumber = parseInt(favNumber.innerHTML)
+    const totalFavNumber = currFavNumber + 1
+    favNumber.innerHTML = totalFavNumber
 }
+
+
+
+document.querySelectorAll(".cardBtn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const card = btn.closest(".card")
+        const serviceName = card.querySelector(".serviceName").innerText
+        const serviceNumber = card.querySelector(".serviceNumber").innerText
+        alert(`📞 Calling ${serviceName} ${serviceNumber}...`)
+
+    })
+
+})
